8768ba38fa78df0ab6c6147ab20253542f160696
Daniel <olmosbritodaniel5@gmail.com>
2026-06-06 21:19:47 -0400

    Revision ciclo5 servidor

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode;
 class AppConfig {
   static const _railwayUrl    = 'https://rutasegurabackend-production.up.railway.app';
   static const _localWeb      = 'http://localhost:8000';
-  static const _localAndroid  = 'http://10.0.2.2:8000';       // emulador
+// emulador
   static const _localFisico   = 'http://192.168.100.7:8000';   // celular físico en red local
 
   static String get baseUrl {
